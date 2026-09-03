acd57aa85e56c663253ebff63831fd62e4275777
dartsoma <chadsomaf@gmail.com>
2026-09-03 00:25:37 -0400

    Revised event_core.h considering int32 overhead and setters for event frame, added implementation for a handful of methods in event_core.c

--- include/event_core.h
+++ include/event_core.h
@@ -1,6 +1,8 @@
 #define __EVENT_CORE_H__
 #ifndef __EVENT_CORE_H__
 
+#include "GFC_HashMap.h"
+
 typedef struct EventData {
     Uint8 type_id;
     size_t data_size; // size of event specific data
@@ -9,22 +11,22 @@ typedef struct EventData {
 
 } EventData;
 
-// primary structure is linked list due to ambigious amount of events
+// Primary structure is linked list due to ambigious amount of events
 
 typedef struct Event {
-    int id; // event ID
-    int frame; // gets the time of initialization in total game frames
+    uint32_t id; // event ID
+    uint32_t frame; // gets the time of initialization in total game frames
     EventData* data; // ambigious data
     struct Event* next; // next in queue for events
-} EventData;
+} Event;
 
-typedef void (*EventCallback)(Event* event, void* event_data);
+typedef void (*EventCallback)(Event* event, void* context);
 
 typedef struct EventListener {
     Uint8 type_id;
     EventCallback callback; // function to call
     void* context; // context data for callback
-    int priority; // order called, lowest to highest
+    uint32_t priority; // order called, lowest to highest
 } EventListener;
 
 // Catalog of types
@@ -39,15 +41,15 @@ typedef struct EventTypeRegistry {
 typedef struct EventQueue {
     Event* head;
     Event* tail;
-    int count;
-    int max_events;
+    uint32_t count;
+    uint32_t max_events;
 } EventQueue;
 
 typedef struct EventSystem {
     EventQueue queue;
     GFC_HashMap* registry; // holds event types by type_id
     GFC_HashMap* listeners; // holds event listeners by name
-    int next_event_id;
+    uint32_t next_event_id;
     Uint8 running;
 } EventSystem;
 
@@ -55,47 +57,49 @@ void event_system_init(EventSystem* system, uint32_t max_queue_size);
 
 void event_system_shutdown(EventSystem* system);
 
-// register new event
+// Register new event
 uint32_t event_register_type(EventSystem* system,
     const char* name,
     size_t data_size,
     void (*cleanup)(void* data));
 
-// create a new event (with data)
+// Create a new event (with data)
 Event* event_create(EventSystem* system,
     uint32_t event_type_id,
     void* data,
-    size_t data_size);
+    size_t data_size,
+    uint32_t frame);
 
-// push event to queue (thread-safe)
-bool event_push(EventSystem* system, Event* event);
+// Push event to queue (thread-safe)
+// Side note: this project is not multithreaded that may come years later
+Uint8 event_push(EventSystem* system, Event* event);
 
-// process all events in queue
-void event_process_queue(EventSystem* system);
+// Process all events in queue
+Uint8 event_process_queue(EventSystem* system);
 
-// add a listener for an event type
-bool event_add_listener(EventSystem* system,
+// Add a listener for an event type
+Uint8 event_add_listener(EventSystem* system,
     uint32_t event_type_id,
     EventCallback callback,
     void* user_data,
     int priority);
 
-// remove a listener
-bool event_remove_listener(EventSystem* system,
+// Remove a listener
+Uint8 event_remove_listener(EventSystem* system,
     uint32_t event_type_id,
     EventCallback callback,
     void* user_data);
 
-// create and push event in one call
-bool event_emit(EventSystem* system,
+// Create and push event in one call
+Uint8 event_emit(EventSystem* system,
     uint32_t event_type_id,
     void* data,
     size_t data_size);
 
-// event type ID frome name
+// Event type ID frome name
 Uint8 event_get_type_id(EventSystem* system, const char* name);
 
-// check if event type exists
-bool event_type_exists(EventSystem* system, Uint8 type_id);
+// Check if event type exists
+Uint8 event_type_exists(EventSystem* system, Uint8 type_id);
 
 #endif
--- src/ui_core.c
+++ src/ui_core.c
@@ -1,3 +1,2 @@
+#include "ui_core.h"
 #include "simple_logger.h"
-
-
